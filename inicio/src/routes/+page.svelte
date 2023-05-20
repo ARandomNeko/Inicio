8fd0ec472d1289f57a746467ffcebec43faffece
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-05-21 00:22:31 +0530

    vite

--- inicio/src/routes/+page.svelte
+++ inicio/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="font-mono text-black">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="bg-black ">
+    <h1 class="font-mono text-white grid text-center ">Welcome to Inicio</h1>
+    <p class="font-mono text-white  text-center">Visit <a class="text-orange-400 underline underline-offset-1" href="https://www.instagram.com/worldpeace_today/">Our instagram</a> to know more</p>
+</section>
+
