85ee2006ee410e2f7b8bafc35b675ca7786c6b34
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-05-21 13:24:11 +0530

    idk

--- inicio/src/routes/+page.svelte
+++ inicio/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <section class="bg-black ">
     <h1 class="font-mono text-white grid text-center ">Welcome to Inicio</h1>
-    <p class="font-mono text-white  text-center">Visit <a class="text-orange-400 underline underline-offset-1" href="https://www.instagram.com/worldpeace_today/">Our instagram</a> to know more</p>
+    <p class="font-mono text-white  text-center">Visit 
+        <a class="text-orange-400 underline underline-offset-1" href="https://www.instagram.com/worldpeace_today/"> 
+            Our instagram</a>
+         to know more</p>
 </section>
 
