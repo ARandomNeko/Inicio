0d6e3f3a2af991fbf7c523c8dc75f3989d624cbb
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-06-11 15:26:19 +0530

    just making the code look better cuz im bored

--- inicio/src/routes/+page.svelte
+++ inicio/src/routes/+page.svelte
@@ -23,14 +23,13 @@
     
         <div class="basis-3/4 text-black mt-6" style="font-family: Montserrat;"> 
             <p class="md:para-scale">
-                World Peace Today is committed to spreading awareness on social issues and current events. Alongside our core mission, we recognize the pressing need to address world hunger and provide meals to underprivileged communities. To this end, we have intensified our focus on this critical area.
-    
+                World Peace Today is committed to spreading awareness on social issues and current events. Alongside our core mission, we recognize the pressing need to address world hunger and provide meals to underprivileged communities. To this end, we have intensified our focus on this critical area.    
                 <br><br>
-                Believing that access to nutritious meals is a fundamental human right, we actively combat hunger. Partnering with local community kitchens, we work to ensure the efficient and equitable distribution of food resources. By leveraging their expertise and networks, we can reach underserved populations and provide sustenance to the most vulnerable individuals and families affected by food insecurity.
-                
-                <br><br>Our efforts extend beyond meal provision. We undertake educational activities to raise awareness about the global hunger crisis, informing individuals about its magnitude, root causes, and potential solutions. By fostering understanding and empathy, we seek to mobilize support and inspire collective action to address hunger, such as through organizing fundraising events. Together, we can create a world where no one goes to bed hungry.
-                
-                <br><br>World Peace Today remains steadfast in its dedication to promoting awareness, advocating for change, and taking concrete actions to alleviate hunger. By shedding light on this critical issue and helping those in need, we strive to build a brighter and nourished future for all.
+                Believing that access to nutritious meals is a fundamental human right, we actively combat hunger. Partnering with local community kitchens, we work to ensure the efficient and equitable distribution of food resources. By leveraging their expertise and networks, we can reach underserved populations and provide sustenance to the most vulnerable individuals and families affected by food insecurity.                
+                <br><br>
+                Our efforts extend beyond meal provision. We undertake educational activities to raise awareness about the global hunger crisis, informing individuals about its magnitude, root causes, and potential solutions. By fostering understanding and empathy, we seek to mobilize support and inspire collective action to address hunger, such as through organizing fundraising events. Together, we can create a world where no one goes to bed hungry.                
+                <br><br>
+                World Peace Today remains steadfast in its dedication to promoting awareness, advocating for change, and taking concrete actions to alleviate hunger. By shedding light on this critical issue and helping those in need, we strive to build a brighter and nourished future for all.
                 
             </p>
         </div>
