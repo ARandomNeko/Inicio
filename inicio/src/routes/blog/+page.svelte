a43dd2759fd6c0afc7330efa240f027b0f7f3b4f
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-07-13 16:27:44 +0530

    just the header and footer left

--- inicio/src/routes/blog/+page.svelte
+++ inicio/src/routes/blog/+page.svelte
@@ -1,4 +1,5 @@
 <section style="background: rgb(250,235,215);" class="h-screen">
     <h1 class="text-center"> Blog </h1> 
-    <p class="text-center"> insert blog placement text/description here </p>                     
+    <p class="text-center"> Welcome to the World Peace Today blog,</p>      
+                   
 </section>
