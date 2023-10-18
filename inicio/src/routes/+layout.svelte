365b150667c2a33707280c7053c7d1fcac4e8e3c
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-10-18 14:07:55 +0530

    fixed routing

--- inicio/src/routes/+layout.svelte
+++ inicio/src/routes/+layout.svelte
@@ -11,7 +11,7 @@
                 <a href="/" class="block  py-2 pr-4 pl-3 text-gray-700  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 rounded-lg">Home</a>
             </li>
             <li>
-                <a href="blog" class="block py-2 pr-4 pl-3 text-gray-700  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 rounded-lg">Blog</a>
+                <a href="/blog/" class="block py-2 pr-4 pl-3 text-gray-700  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 rounded-lg">Blog</a>
             </li>
 
         </ul>
