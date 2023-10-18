34862ab5cf0fcfaf2664c91b1bcdf39d350d95e8
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-10-18 11:55:42 +0530

    im fucking done

--- inicio/src/routes/blog/+page.svelte
+++ inicio/src/routes/blog/+page.svelte
@@ -9,22 +9,22 @@
             <div class=" md:grid grid-cols-2 grid-rows-2 md:px-10 px-6">
                 <!-- ONE -->
                 <a href="/blog/1" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 md:ml-6 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(131,223,215,0.8);">
+                    <img src="/assets/food-insec.png" class="cropped rounded-2xl">
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                            Food Insecurity The World's Hunger Problem
+                        </b>
+                       <br>By - Rishiki Goteti
+                    </p> 
+                </a>
+                <!-- TWO -->
+                <a href="/blog/2" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(203,216,211,0.8);">
                     <img src="https://www.cychacks.com/wp-content/uploads/2020/06/How-Technology-is-Helping-Fight-against-Hunger.jpg" class="cropped rounded-2xl">
                     <p class="text-left ml-6 my-auto md:scale">
                         <b>
                             Technology's Role in the Fight Against World Hunger
                         </b>
-                       <br>By - Shouri Singaraju
-                    </p> 
-                </a>
-                <!-- TWO -->
-                <a href="/blog/2" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(203,216,211,0.8);">
-                    <img src="/assets/nishka.jpeg" class="cropped rounded-2xl">
-                    <p class="text-left ml-6 my-auto md:scale">
-                        <b>
-                           
-                        </b>
-                       <br>By - Rishika Goteti
+                       <br>By -  Shouri Singaraju
                     </p> 
                 </a>
                 <!-- THREE -->
