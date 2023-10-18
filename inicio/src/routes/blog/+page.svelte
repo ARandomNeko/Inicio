f1a269cec4c5c62dffa80faa010f42725bcb6f1d
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-10-18 15:01:25 +0530

    bg

--- inicio/src/routes/blog/+page.svelte
+++ inicio/src/routes/blog/+page.svelte
@@ -1,4 +1,4 @@
-<section style="background: rgb(250,235,215);" class="h-screen">
+<section class="h-screen">
     <h1 class="text-center"> Blog </h1> 
     <p class="text-center"> Welcome to the World Peace Today blog</p>      
     
