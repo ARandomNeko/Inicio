3ea128c658d06728fe892259f3feb9ecceb082e6
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-10-22 12:51:28 +0530

    blog done

--- inicio/src/routes/blog/+page.svelte
+++ inicio/src/routes/blog/+page.svelte
@@ -6,20 +6,20 @@
         <!--Meet the team-->
         <div class=""> 
                                
-            <div class=" md:grid grid-cols-2 grid-rows-2 md:px-10 px-6">
+            <div class=" md:grid grid-cols-2  md:px-10 px-6">
                 <!-- ONE -->
                 <a href="/blog/1" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 md:ml-6 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(131,223,215,0.8);">
-                    <img src="/assets/food-insec.png" class="cropped rounded-2xl">
+                    <img src="https://www.ucsf.edu/sites/default/files/styles/2015_news_center_rotator/public/field/image/news/food-insecurity.jpg" class="cropped rounded-2xl my-auto">
                     <p class="text-left ml-6 my-auto md:scale whitespace-normal">
                         <b>
                             Food Insecurity The World's Hunger Problem
                         </b>
-                       <br>By - Rishiki Goteti
+                       <br>By - Rishika Goteti
                     </p> 
                 </a>
                 <!-- TWO -->
                 <a href="/blog/2" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(203,216,211,0.8);">
-                    <img src="https://www.cychacks.com/wp-content/uploads/2020/06/How-Technology-is-Helping-Fight-against-Hunger.jpg" class="cropped rounded-2xl">
+                    <img src="https://techstory.in/wp-content/uploads/2016/07/technology-fight-hunger-cover-e1467650925981.jpg" class="cropped rounded-2xl my-auto" >
                     <p class="text-left ml-6 my-auto md:scale">
                         <b>
                             Technology's Role in the Fight Against World Hunger
@@ -28,7 +28,65 @@
                     </p> 
                 </a>
                 <!-- THREE -->
-
+                <a href="/blog/3" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(231,224,86,0.8);">
+                    <img src="https://www.htworld.co.uk/wp-content/uploads/2022/08/AdobeStock_317604714_Editorial_Use_Only-2048x1152.jpeg" class="cropped rounded-2xl my-auto" >
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                            Amazon Shut Down
+                        </b>
+                       <br>By -  Anika Vadala
+                    </p> 
+                </a>
+                <!-- FOUR -->
+                <a href="/blog/4" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(164,220,219,0.8);">
+                    <img src="https://static01.nyt.com/images/2018/10/18/world/18afghan-moment-jp-1-print/18afghan-moment-jp-1-print-videoSixteenByNine3000-v2.jpg?year=2018&h=1688&w=3000&sig=0xb9c4914557d766fa00205a31357f622d&tw=1" class="cropped rounded-2xl my-auto" >
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                            Crisis in Afghanistan
+                        </b>
+                       <br>By -  Anika Vadala
+                    </p> 
+                </a>
+                <!-- FIVE -->
+                <a href="/blog/5" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(203,216,211,0.8);">
+                    <img src="https://imgix.bustle.com/mic/etq0msbut6kgpqjobwpeseig14wyw91erykxzzzimtefbdfbi7u03mutbttwssay.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70&dpr=2" class="cropped rounded-2xl my-auto" >
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                            Gender Neutral Uniforms 
+                        </b>
+                       <br>By -  Siri Adusimilli
+                    </p> 
+                </a>
+                <!-- SIX -->
+                <a href="/blog/6" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(233,185,160,0.8);">
+                    <img src="https://cdn.britannica.com/47/1947-050-68FB6BE0/Flag-North-Korea.jpg" class="cropped rounded-2xl my-auto" >
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                         North Korea Makes Threats of Nuclear Bombing    
+                        </b>
+                       <br>By -  Siri Adusimilli
+                    </p> 
+                </a>
+                <!-- SEVEN -->
+                <a href="/blog/7" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(233,185,160,0.8);">
+                    <img src="https://rclutz.files.wordpress.com/2019/04/article11803.large_.jpg?w=2000 " class="cropped rounded-2xl my-auto" >
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                         Drastic Floods in Bangalore
+                        </b>
+                       <br>By -  Anika Vadala
+                    </p> 
+                </a>
+                 <!-- EIGHT -->
+                <a href="/blog/8" class="text-center mt-6 rounded-2xl p-6 grid grid-cols-2 sm:mx-3 -mx-3 hover:scale-105 transition-all hover:shadow-xl" style="background-color: rgba(203,216,211,0.8);">
+                    <img src="https://ginovus.com/wp-content/uploads/iStock-872649056-Bridge-the-Gap-2.jpg" class="cropped rounded-2xl my-auto" >
+                    <p class="text-left ml-6 my-auto md:scale">
+                        <b>
+                         Bridging the Gap
+                        </b>
+                       <br>By -  Arya Sunkara
+                    </p> 
+                </a>
             </div>
         </div>
     
